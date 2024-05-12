7c6844cfbfab2fa2afd4bdd16f2fc2076588dcfa
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-12 19:29:44 +0800

    feat: Add AWS Lambda permission for S3 bucket access

--- lambda.tf
+++ lambda.tf
@@ -17,3 +17,11 @@ resource "aws_lambda_function" "notify_update" {
     }
   }
 }
+
+resource "aws_lambda_permission" "allow_s3_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.notify_update.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.software_bucket.arn
+}
