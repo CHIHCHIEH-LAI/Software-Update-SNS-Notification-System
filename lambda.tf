40d5cc84d71388a9d5b94266dbec30325b6f50c0
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-12 14:57:24 +0800

    feat: Add AWS Lambda function for sending SNS notifications

---
+++ lambda.tf
@@ -0,0 +1,19 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "notify_update" {
+  function_name = "notify_update"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  filename      = data.archive_file.lambda_zip.output_path
+
+  environment {
+    variables = {
+      SNS_TOPIC_ARN = aws_sns_topic.software_updates.arn
+    }
+  }
+}
